fbb54055bac26f3f1b04f3e031b9018646907339
soulman7 <hgautam846@gmail.com>
2019-05-26 14:16:11 +0530

    second commit

--- stock/stock.py
+++ stock/stock.py
@@ -6,16 +6,18 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///person.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///stock.db'
 
 db =SQLAlchemy(app)
 
 stock_url ='https://api.worldtradingdata.com/api/v1/stock?symbol={}&api_token=FZKP0jcyRUsVimi1RSuTQJT4VFy1RmhxyIb5OrCZiWJE8oZA0SSkkWtWgg1w'
 intra_day ='https://intraday.worldtradingdata.com/api/v1/intraday?symbol={}&range=1&interval=1&api_token=FZKP0jcyRUsVimi1RSuTQJT4VFy1RmhxyIb5OrCZiWJE8oZA0SSkkWtWgg1w'
 
-class Person(db.Model):
+class Stock(db.Model):
     id =db.Column(db.Integer ,primary_key=True)
-    data =db.Column(db.String(50) ,nullable=False)
+    balance =db.Column(db.String(50) ,nullable=False)
+    quote =db.Column(db.String(50) ,nullable=False)
+    shares =db.Column(db.String(50) ,nullable=False)
 
 @app.route('/',methods=['GET' ,'POST']) 
 def main():
@@ -29,11 +31,11 @@ def main():
             if 'Message' in r.keys():
                 f=1
             else:
-                person_obj =Person(data =str(r['data'][0]['price']))
-                db.session.add(person_obj)
+                stock_obj =Stock(data =str(r['data'][0]['price']))
+                db.session.add(stock_obj)
                 db.session.commit()
 
-    person = Person.query.all()
+    stock = Stock.query.all()
     stock_data=[]
     for i in person:
         stock_data.append(i.data)
